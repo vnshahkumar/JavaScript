c2eef606fc50774da070ad802170647b1a0e489b
Avinash Kumar <147942952+vnshahkumar@users.noreply.github.com>
2023-10-15 10:50:44 +0000

    ConversionOperation

---
+++ 01_Basic/03_ConversionOperation.js
@@ -0,0 +1,125 @@
+/*
+let score=33;
+console.log(typeof score);//number
+console.log(typeof(score));//number
+
+
+
+
+console.log("-----------------------------------------");
+
+
+
+
+let scores="22";
+console.log(typeof scores);//String
+console.log(typeof(scores));//String
+console.log("----------------");
+//Conversion  String into number
+let valueInNumber=Number(scores);
+console.log(typeof valueInNumber);//number
+console.log(valueInNumber);//22
+
+
+
+console.log("-------------------String-----------------------");
+
+
+
+let Strings="22abc";
+console.log(typeof Strings);//String
+console.log(Strings);//22abc
+let number=Number(Strings);
+console.log(typeof number);//number
+console.log(number);//(NaN)not a number
+
+
+
+
+console.log("-----------------null--------------------------");
+
+
+
+let object=null;
+console.log(typeof object);//object
+console.log(object);//null
+console.log("-------------")
+let object0=Number(object);
+console.log((object0));//   0
+console.log(typeof object0);//number
+
+
+
+console.log("--------------------Undefined ------------------------")
+
+
+
+
+let type=undefined;
+console.log(typeof type);//undefined 
+console.log(type);//undefined
+console.log("--------------")
+let type0=Number(type);
+console.log(typeof type);//undefined
+console.log(typeof(type));//undefined
+
+
+console.log("----------------------Boolean(true)----------------------")
+
+
+let boolean=true;
+console.log(typeof boolean);//boolean
+console.log(boolean);//true
+console.log("-----------------")
+let boolean1=Number(boolean);
+console.log(typeof boolean1);//number
+console.log(boolean1);//1
+
+
+console.log("----------------------Boolean(False)-------------------------")
+
+
+
+let boolean0=false;
+console.log(typeof boolean0);//boolean
+console.log(boolean0);//false
+console.log("------------");
+let boolean2=Number(boolean0);
+console.log(typeof boolean2);//number
+console.log(boolean2);//0
+
+conole.log("-------------------------------------------------")
+
+let isLoggedin=1;
+let booleanIsLoggedIn=Boolean(isLoggedin);
+console.log(typeof booleanIsLoggedIn);//boolean
+console.log(booleanIsLoggedIn);//true
+console.log("---------------");
+let isNotLoggedin=0;
+let booleanIsNotLoggedIn=Boolean(isNotLoggedin);
+console.log(typeof booleanIsNotLoggedIn);//boolean
+console.log(booleanIsNotLoggedIn);//false
+
+
+
+
+console.log("-----------------------------------------------")
+
+
+
+let EmptyString="";
+console.log(EmptyString);//Empty
+ 
+let emptyString=Boolean(EmptyString);
+console.log(typeof emptyString);//boolean
+console.log(emptyString);//false
+
+*/
+
+
+let someNumber=22;
+let StringNumber=String(someNumber);
+console.log(typeof StringNumber);//String
+console.log(StringNumber);//22
+
+
