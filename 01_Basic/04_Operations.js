d89a98469d04e8e088e0e8fdb8385e3708b16c24
Avinash Kumar <147942952+vnshahkumar@users.noreply.github.com>
2023-10-15 11:28:59 +0000

    Operations Test

---
+++ 01_Basic/04_Operations.js
@@ -0,0 +1,32 @@
+//****************Operations On Values**********************/
+let value=4;
+let negValue=-value;
+console.log(value);
+console.log(negValue)
+
+
+
+//**************************** */
+
+
+console.log(4+4);//8
+console.log(2-2);//0
+console.log(2*2);//4
+console.log(2**3);//8
+console.log(2/3);//0.6666666666
+console.log(2%4);//2
+
+
+//*************************************************** */
+let str1="Avinash";
+let str2=" Kumar";
+let str3=str1+str2;
+console.log(str3);
+//*********************************************** */
+
+console.log("1"+2);//12
+console.log(2+"1");//21
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+console.log(1+"2"+2);//122
+console.log(1+"2"+2+"2"+2+"2"+2);//1222222
