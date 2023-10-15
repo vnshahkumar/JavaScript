4f551d7a5a5498675944ed42ecfc1a15be58ab72
Avinash Kumar <147942952+vnshahkumar@users.noreply.github.com>
2023-10-15 09:27:13 +0000

    datatype

---
+++ 01_Basic/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict"//treat all js code as newer version 
+
+
+// alert(3+3);//we are using node js ,not browser 
+
+console.log(3 + 3);
+console.log("Avinash kumar");
+
+
+
+
+let name="hitesh";
+let age=123;
+let isLoggedin=false;
+console.table([name,age,isLoggedin]);
+
+
+
+//object 
+console.log(typeof undefined);//it is a type undefined
+console.log(typeof null);// it is a object 
