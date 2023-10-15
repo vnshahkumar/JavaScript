9ad7f16b9e52c92c8143ff3ebb26e622618e238d
Avinash Kumar <147942952+vnshahkumar@users.noreply.github.com>
2023-10-15 09:28:34 +0000

    Variables

---
+++ 01_Basic/01_Variables.js
@@ -0,0 +1,21 @@
+const accountId=1234567890;
+let accountemail="vnavinasdb@gmail.com";
+var accountPassword="123456";
+accountCity="Vaishali";
+let accountState;
+
+// accountId=345456;//const cannot be chang
+console.log(accountId);
+
+
+
+/*
+what is os module (Operating system module )
+when we have need to know our operating 
+system information
+*/
+
+accountPassword="7647678";
+accountCity="bihar";
+accountemail="GFFHFHBn@546b";
+console.table([accountId,accountPassword,accountemail,accountCity,accountState]);
