d55f4d0f23dfa3b7877ebf85d065fe009ee6c96c
Avinash Kumar <147942952+vnshahkumar@users.noreply.github.com>
2023-10-18 22:39:59 +0000

    control flow

---
+++ 05_Control_flow/01_one.js
@@ -0,0 +1,62 @@
+//if
+if(true){
+
+    // console.log("Code excequt");
+}
+if(false){
+    // console.log("Code not excequted");
+}
+
+const isUserLoggedIn=true
+if(isUserLoggedIn){
+    // console.log("Code excequte");
+}
+//< , > , <= ,=> ,== , != ,=== !==
+if(2!=3){
+    // console.log("Executed");
+}
+const Temp=45;
+if(Temp<50){
+    // console.log("yes Temp less then 50");
+}
+else{
+
+    // console.log("Temperature is greater then 50")
+}
+
+const score = 200
+if (score > 100) {
+let power = "fly"
+// console.log(`User power: ${power}`);
+ }
+
+ // const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
